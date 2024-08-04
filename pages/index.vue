27588b367a8f6c638966d18e2be45c88cddd2782
Joshua <joshua.noakes1@icloud.com>
2024-08-04 01:54:00 +0100

    nuxt and vantajs

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import * as THREE from 'three';
+
+onMounted(async () => {
+    if (import.meta.browser) {
+        window.THREE = THREE;
+        const { default: CLOUDS } = await import("vanta/dist/vanta.clouds.min");
+        CLOUDS({
+            el: "#vanta",
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            THREE: window.THREE
+        });
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h1>:3</h1>
+        <div id="vanta" style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: -100;"></div>
+    </div>
+</template>
