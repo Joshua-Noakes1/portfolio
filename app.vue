e9b8120c9c6dd28925fcdbd7f496444c241ff3da
Joshua <joshua.noakes1@icloud.com>
2024-08-05 12:50:58 +0100

    add nuxt ui

--- app.vue
+++ app.vue
@@ -1,6 +1,28 @@
+<script setup>
+import * as THREE from 'three';
+
+onMounted(async () => {
+  if (import.meta.browser) {
+    const { default: CLOUDS } = await import("vanta/dist/vanta.clouds.min");
+    CLOUDS({
+      el: "#vantajs",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      THREE: THREE
+    });
+  }
+});
+</script>
+
 <template>
   <div>
+    <NavBar />
+    <NuxtLoadingIndicator />
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
+  <div id="vantajs" style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: -100;" />
 </template>
---
+++ components/NavBar/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <UContainer>
+        <UCard class="mt-5">
+            <template #header>
+                <div class="flex justify-between">
+                    <NuxtLink to="/">
+                        <h1 class="font-semibold">Joshua</h1>
+                    </NuxtLink>
+                    <span>
+                        <UButton class="md:visible invisible" to="https://res.cloudinary.com/tigersoot/image/upload/v1722800242/cv.pdf" target="_blank">
+                            <Icon name="grommet-icons:github" size="20px" />
+                        </UButton>
+                        <!-- <UButton class="ml-1" :title="`Current Colour Prefrence: ${$colorMode.preference.toString().charAt(0).toUpperCase()}${$colorMode.preference.toString().slice(1).toLowerCase()}`" @click="switchColour()">
+                            <Icon :name="`heroicons:${$colorMode.preference === 'system' ? 'computer-desktop-20-solid' : $colorMode.value === 'dark' ? 'moon' : 'sun'}`" size="20px" />
+                        </UButton>
+                        <UButton v-if="isLoggedIn" class="ml-1" @click="signUserOut()" target="_blank" title="Log Out">
+                            <Icon name="solar:square-arrow-right-broken" size="20px" />
+                        </UButton> -->
+                    </span>
+                </div>
+            </template>
+        </UCard>
+    </UContainer>
+</template>
